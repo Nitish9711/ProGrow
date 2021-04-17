1e221b5e15c762f6a99fbd8c568cffa185e0f42b
Nitish9711 <nitishkumar12c@gmail.com>
2021-04-17 06:15:22 +0530

    more things were added

--- index.js
+++ index.js
@@ -1,100 +1,92 @@
 const express = require('express');
-const router = express.Router();
-const authentication = require('../middleware/authentication');
-const authorization = require('../middleware/authorization');
+const app = express();
+const mongoose = require('mongoose');
+const session = require('express-session');
+const path = require('path');
 const passport = require('passport');
-const wrapAsync = require('../utils/wrapAsync');
-const Contractor = require('../models/Contractor');
-const upload = require('../config/multer');
-const fs = require('fs').promises;
-const Stock = require("../models/Stock");
-const Sold = require("../models/Sold");
+const PassportLocal = require('passport-local');
+const AppError = require('./utils/AppError');
+const Contractor = require('./models/Contractor');
+const Farmer = require('./models/Farmer');
+const contractorsRouter = require('./routes/contractors');
+const farmersRouter = require('./routes/farmers');
+const authentication = require('./middleware/authentication');
 
-router.get('/login',authentication.ensureNoLogin,(req,res) => {
-    res.render('contractors/login');
+(async function(){
+    try{
+        await mongoose.connect('mongodb+srv://nitish_kumar:1234567890@cluster0.xt7ds.mongodb.net/progrow?retryWrites=true&w=majority',{
+            useNewUrlParser: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB is running...');
+    }catch(err){
+        console.log(err);
+        process.exit(1);
+    }
+})();
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({extended:true}));
+
+app.use(session({
+    name: 'session',
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        maxAge: 1000*60*60*24*7
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use('farmerLocal',new PassportLocal(Farmer.authenticate()));
+passport.use('contractorLocal',new PassportLocal(Contractor.authenticate()));
+passport.serializeUser(function(user,done){
+    done(null,user);
+});
+passport.deserializeUser(function(user,done){
+    if(user) done(null,user);
+})
+
+app.use((req,res,next) => {
+    res.locals.user = req.user;
+    next();
+})
+
+app.get('/',(req,res) => {
+    res.render('landing');
 });
 
-router.post('/login',authentication.ensureNoLogin,passport.authenticate('contractorLocal',{failureRedirect: '/contractors/login'}),(req,res) => {
-    res.redirect('/contractors/dashboard');
+app.use('/farmers',farmersRouter);
+app.use('/contractors',contractorsRouter);
+
+app.post('/logout',authentication.ensureLogin,(req,res) => {
+    req.logout();
+    res.redirect('/');
 })
 
-router.get('/signup',authentication.ensureNoLogin,(req,res) => {
-    res.render('contractors/signup');
+app.get('/about',(req,res) => {
+    res.render('about');
 })
 
-router.post('/',authentication.ensureNoLogin,upload.single('image'),wrapAsync(async (req,res) => {
-    const {password} = req.body;
-    const contractor = new Contractor(req.body);
-    if(req.file) contractor.image = `/images/uploads/${req.file.filename}`;
-    try{
-        await Contractor.register(contractor,password);
-        req.login(contractor,err => {
-            if(err) throw err;
-            else res.redirect('/contractors/dashboard');
-        })
-    }catch(err){
-        if(req.file) await fs.unlink(`/images/uploads/${req.file.filename}`);
-        throw err;
-    }
-}))
-
-router.get('/dashboard',authentication.ensureLogin,authorization.ensureContractor,(req,res) => {
-    res.render('contractors/dashboard');
-})
-router.post('/dashboard/stock',(req,res) => {
-    
-    
-    console.log("data");
-    Stock.find({}, function(err, data) {
-        console.log(data);
-        res.send(data);
-    });
-    
-
+app.get('/contact',(req,res) => {
+    res.render('contact');
 })
 
-router.post('/buyStock',(req,res) => {
-    qty = req.body.qty;
-    _id = req.body._id;
-//,authentication.ensureLogin,authorization.ensureContractor
-    Stock.findById(_id)
-    .then(data => {
-        const newStock = data;
-        console.log(newStock)
-        newStock.qty.amount = newStock.qty.amount - qty;
-        
-        newQty = {
-            amount: qty,
-            unit: data.qty.unit
-        }
-        const sold = new Sold({
-            // farmer : data.farmer,
-            // contractor : req.user._id,
-            qty : newQty,
-            price : qty * data.price,
-        });
-        sold.save;
-        Stock.findByIdAndUpdate(_id, newStock, {upsert: true});
-    }).catch(err => console.log(err));
-    res.json({msg:"success"})
+app.use((req,res) => {
+    throw new AppError('Not found', 404);
 })
 
+app.use((err,req,res,next) => {
+    if(!err.status) err.status = 500;
+    if(!err.message) err.message = 'Something went wrong';
+    res.status(err.status).render('error',{err});
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = router;
+const port = 3000;
+app.listen(port,() => console.log(`Server is running on port ${port}...`)); 
